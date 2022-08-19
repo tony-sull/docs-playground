c8d8ee67d063e64a86b598d4308385250b616b69
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-08-18 22:27:09 -0400

    adding current page styles

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -17,12 +17,12 @@ export function isHeader(item: SidebarItem): item is SidebarHeader {
 const sidebar: { [lang: string]: SidebarItem[] } = {
 	en: [
 		{ text: 'Section Header', header: true },
-		{ text: 'Introduction', link: '/en/introduction' },
-		{ text: 'Page 2', link: '/en/page-2' },
-		{ text: 'Page 3', link: '/en/page-3' },
+		{ text: 'Introduction', link: '/introduction' },
+		{ text: 'Page 2', link: '/page-2' },
+		{ text: 'Page 3', link: '/page-3' },
 
 		{ text: 'Another Section', header: true },
-		{ text: 'Page 4', link: '/en/page-4' },
+		{ text: 'Page 4', link: '/page-4' },
 	],
 }
 
